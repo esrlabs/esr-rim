3a5b6d96ca29421c5580171e756a6a12b69a519e
thomas.herrmann <thomas.herrmann@partner.esrlabs.com>
2015-03-02 17:02:46 +0100

    Added synch/upload tests to unit tests.

--- test/unit_tests.rb
+++ test/unit_tests.rb
@@ -2,6 +2,10 @@ $:.unshift File.join(File.dirname(__FILE__))
 
 # include all unit tests here
 require 'manifest_helper_test'
+require 'file_helper_test'
 require 'rim_info_test'
 require 'dirty_check_test'
 require 'status_builder_test'
+require 'sync_helper_test'
+require 'sync_module_helper_test'
+require 'upload_module_helper_test'
